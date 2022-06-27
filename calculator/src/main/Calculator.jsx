eee7bdd53e17ef3e40e8b68ddadc5a514d0e487a
lazhcsilva <lazhcsilva@gmail.com>
2022-06-27 18:53:14 -0300

    Creating button components

--- calculator/src/main/Calculator.jsx
+++ calculator/src/main/Calculator.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './Calculator.css';
 
+import Button from '../component/Button';
+
 export default class Calculator extends Component {
     render() {
         return (
             <div className="calculator">
-
+                <Button />
             </div>
         )
     }
